85cc2f9489894a2b7966cee0c1905d2c75b36985
Julien Parlant <julien.parlant@gmail.com>
2025-09-11 17:26:41 +0200

    Chakra configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
     <App />
-  </StrictMode>,
-)
+  </ChakraProvider>
+);
